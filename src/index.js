a9ef2f356249933a73f0e4cc347006a3320d2a17
Yashpal2004 <meyashpal2004@gmail.com>
2024-02-21 16:50:11 +0530

    newCard

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 
-let card
+let cards = []
 
 // Decalaration/Initialisation of DOM
 const content = document.getElementById("content")
@@ -7,34 +7,38 @@ const allButton = document.getElementById("all")
 const newButton = document.getElementById("new")
 
 
-//EventListners
-allButton.addEventListener("click", allCards())
-newButton.addEventListener("click", newCard())
+const form = document.createElement("form")
+const tl = document.createElement("label")
+tl.setAttribute("for", "title")
+tl.setAttribute("class", "label")
+tl.innerText = "Title"
+const ti = document.createElement("input")
+ti.setAttribute("type", "text")
+ti.setAttribute("name", "title")
+ti.setAttribute("id", "title")
+const dl = document.createElement("label")
+dl.setAttribute("for", "description")
+dl.setAttribute("class", "label")
+dl.innerText = "Description"
+const di = document.createElement("input")
+di.setAttribute("type", "text")
+di.setAttribute("name", "description")
+di.setAttribute("id", "description")
+const submitButton = document.createElement("button")
+submitButton.type = "button"
+submitButton.innerText = "Submit"
 
+
+
+//EventListners
+allButton.addEventListener("click", allCards)
+newButton.addEventListener("click", newCard)
+
+allCards()
 
 //functions
 function newCard() {
   content.innerHTML = ""
-  const form = document.createElement("form")
-  const tl = document.createElement("label")
-  tl.setAttribute("for", "title")
-  tl.setAttribute("class", "label")
-  tl.innerText = "Title"
-  const ti = document.createElement("input")
-  ti.setAttribute("type", "text")
-  ti.setAttribute("name", "title")
-  ti.setAttribute("id", "title")
-  const dl = document.createElement("label")
-  dl.setAttribute("for", "description")
-  dl.setAttribute("class", "label")
-  dl.innerText = "Description"
-  const di = document.createElement("input")
-  di.setAttribute("type", "text")
-  di.setAttribute("name", "description")
-  di.setAttribute("id", "description")
-  const submitButton = document.createElement("button")
-  submitButton.type = "button"
-  submitButton.innerText = "Submit"
 
   form.appendChild(tl)
   form.appendChild(ti)
@@ -43,11 +47,12 @@ function newCard() {
   form.appendChild(submitButton)
   content.appendChild(form)
 
-  submitButton.addEventListener("click", submitValues(ti, di))
+  submitButton.addEventListener("click", submitValues)
 }
 
-function submitValues(ti, di) {
-  card = {title:ti.value, description:di.value}
+function submitValues() {
+  let card = {title:ti.value, description:di.value}
+  cards.push(card)
   console.log(card)
 }
 
